ec8d960b3665ce9e2f2e121ce1a7d961ff280ced
Yangxg <jessebutchman@gmail.com>
2017-08-21 23:14:18 +0800

    1

--- test/readdir.js
+++ test/readdir.js
@@ -4,14 +4,27 @@ var Path = require('path')
 
 // 遍历当前目录
 
-function pathToJSON(path){
-	loadDir
+async function pathToJSON(path,deep=0){
 	// 返回指定目录下所有文件
-
+	let current = await loadDir(path)
 	// 检测目录是否文件夹
-
+	for(i in current){
+		let status = await stat(path+'/'+current[i])
+		console.log(path+'/'+current[i])
+		// console.log()
+		if(status.isDirectory()){
+			await pathToJSON(path+'/'+current[i],++deep)
+		}
+	}
 }
 
+function formatSpace(deep){
+	let res = "-"
+	for (var i = 0; i <deep; i++) {
+		res += "-"
+	}
+	return res
+}
 // 读取指定目录所有文件
 async function loadDir(path){
 	return new Promise(function(reslove,reject){
@@ -25,6 +38,11 @@ async function loadDir(path){
 }
 async function stat(path){
 	return new Promise(function(reslove,reject){
-
+		fs.stat(path,function(err,res){
+			if(err){reject(err)}
+			reslove(res)
+		})
 	})
-} 
+}
+// pathToJSON(__dirname+'/../template')
+pathToJSON("C:/Users/jesse/Documents/GitHub/fastInit/vendor")
